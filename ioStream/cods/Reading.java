e80c9aa8dde60b1314d4f4b32dd797b7cd588620
Celtico(Alan) <alanmesquita2022@gmail.com>
2023-07-31 17:36:11 -0300

    31/07/2023

---
+++ ioStream/cods/Reading.java
@@ -0,0 +1,19 @@
+package ioStream.cods;
+/*steps
+ * 1º import java.io
+ * 2º create the outputStream object based on the source
+ * 3º call read to read data
+ * 4º close
+ * 5º handle FNFE and IoException
+ */
+
+import java.io.*; //1º step 
+
+public class Reading {
+
+	public static void main(String[] args) {
+		//FileInputStream inputStream = new FileInputStream();
+
+	}
+
+}
---
+++ ioStream/cods/Writing.java
@@ -0,0 +1,22 @@
+package ioStream.cods;
+
+/*steps
+ * 1º import java.io
+ * 2º create the outputStream object based on the source
+ * 3º call write to save data
+ * 4º use flush
+ * 5º close the stream
+ * 6º handle FNFE and IoException
+ */
+
+import java.io.*; //1º step
+
+public class Writing {
+	
+	public static void main(String[] args) {
+		//FileOutputStream outputStream = new FileOutputStream(); //2º step
+		
+
+	}
+
+}
